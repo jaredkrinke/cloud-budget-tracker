1710d67980ff35d82c0a674aa7ae1b43156c04a3
jaredkrinke <jaredkrinke@gmail.com>
2014-08-12 20:33:34 -0700

    Switch to Express and start data model

--- server/main.js
+++ server/main.js
@@ -1,6 +1,21 @@
-﻿var http = require('http');
-http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello, world!');
-    response.end();
-}).listen(8888);
+﻿var app = require('express')();
+
+// Data model
+var balance = 0;
+var transactions = [];
+
+var addTransaction = function (transaction) {
+    transactions.push(transaction);
+    balance -= transaction.amount;
+};
+
+app.get('/', function (request, response) {
+    response.send(JSON.stringify({
+        balance: balance,
+        transactions: transactions,
+    }));
+});
+
+var server = app.listen(8888, function () {
+    console.log('Listening on port %d...', server.address().port);
+});
