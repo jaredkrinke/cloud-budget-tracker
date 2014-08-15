fcf2de0664bb38445af72f1851ded646e04d9d9a
jaredkrinke <jaredkrinke@gmail.com>
2014-08-14 21:33:00 -0700

    Initial user database

---
+++ server/add-user.js
@@ -0,0 +1,41 @@
+﻿var bcrypt = require('bcryptjs');
+var Datastore = require('nedb');
+var userPattern = /^[a-z][a-z0-9]*$/i;
+
+var argv = process.argv;
+if (argv.length === 4) {
+    var user = argv[2];
+    var password = argv[3];
+
+    // Validate user name
+    if (userPattern.test(user)) {
+        // Hash the password
+        var salt = bcrypt.genSaltSync();
+        var hashedPassword = bcrypt.hashSync(password, salt);
+
+        // Load from the database
+        var db = new Datastore({ filename: __dirname + 'users.db', });
+        db.loadDatabase(function (err) {
+            if (err) {
+                console.log('Error loading database!');
+                console.log(err);
+            } else {
+                console.log('Inserting user: %s...', user);
+                var document = {
+                    user: user.toLowerCase(),
+                    userDisplay: user,
+                    password: hashedPassword,
+                    salt: salt,
+                };
+
+                db.insert(document, function (err, document) {
+                    console.log(document);
+                });
+            }
+        });
+    } else {
+        console.log('Invalid user name: %s', user);
+    }
+} else {
+    console.log('USAGE: <User> <Password>');
+}
